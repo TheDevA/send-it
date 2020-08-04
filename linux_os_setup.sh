96f80f030b84fdd6b0dbe5ccd03f79c821c90fb2
Zer0_Day <54479109+TheDevA@users.noreply.github.com>
2020-08-04 18:25:25 +0200

    Update linux_os_setup.sh

--- linux_os_setup.sh
+++ linux_os_setup.sh
@@ -11,8 +11,8 @@ mkdir ffmpegtemp
 # wget ffmpeg an tar the file
 wget -P ./ffmpegtemp https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
 
-tar xvf ./ffmpegtemp/ffmpeg-git-amd64-static.tar.xz -C ./ffmpegtemp
+tar xvf ./ffmpegtemp/ffmpeg-git-amd64-static.tar.xz -C ./ffmpegtemp --strip-components=1
 
 # move ffmpeg and ffprobe to the proper place
-mv ./ffmpegtemp/ffmpeg-git-20200722-amd64-static/ffmpeg ./app && mv ./ffmpegtemp/ffmpeg-git-20200722-amd64-static/ffprobe ./app
+mv ./ffmpegtemp/ffmpeg ./app && mv ./ffmpegtemp/ffprobe ./app
 
